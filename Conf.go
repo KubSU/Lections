136916ca225769bbb94056dffdaeea873d67d206
mpsnp <kir_george@mail.ru>
2013-10-04 17:16:14 +0400

    Конфиги для gource

---
+++ Conf.go
@@ -0,0 +1,15 @@
+[display]
+output-ppm-stream=Video.ppm
+output-framerate=25
+multi-sampling=true
+viewport=1920x1080
+fullscreen=true
+
+[gource]
+seconds-per-day=1
+title=KOHC?EKT
+user-image-dir=.git/avatar/
+background-colour=000000
+key=true
+bloom-intensity=1
+
